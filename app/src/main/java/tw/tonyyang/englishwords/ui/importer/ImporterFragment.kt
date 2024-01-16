5dfce286f7ceb70267edc925803e481dbfc43eb3
Tony Yang <superzoro168@gmail.com>
2024-01-17 03:07:32 +0800

    remove unnecessary permission request

--- app/src/main/java/tw/tonyyang/englishwords/ui/importer/ImporterFragment.kt
+++ app/src/main/java/tw/tonyyang/englishwords/ui/importer/ImporterFragment.kt
@@ -1,8 +1,6 @@
 package tw.tonyyang.englishwords.ui.importer
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +8,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import dev.shreyaspatil.MaterialDialog.MaterialDialog
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import tw.tonyyang.englishwords.util.Logger
 import tw.tonyyang.englishwords.R
 import tw.tonyyang.englishwords.databinding.FragmentDropboxchooserBinding
 import tw.tonyyang.englishwords.state.Result
@@ -26,55 +21,28 @@ class ImporterFragment : Fragment() {
 
     private lateinit var layout: View
 
+    private lateinit var progress: AlertDialog
+
     private val viewModel: ImporterViewModel by sharedViewModel()
 
     private var fileUrl: String? = null
 
-    private var progress: AlertDialog? = null
-
     private val startChooseFileLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()
-            ) { result ->
-                Logger.d(TAG, "choose file result")
-                val uri = result?.data?.data
-                if (uri != null) {
-                    fileUrl = uri.toString()
-                    binding.tvFilename.text = fileUrl
-                    binding.tvFileSize.text = ""
-                    binding.btnSubmit.isEnabled = true
-                }
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val uri = result?.data?.data
+            if (uri != null) {
+                fileUrl = uri.toString()
+                binding.tvFilename.text = fileUrl
+                binding.tvFileSize.text = ""
+                binding.btnSubmit.isEnabled = true
             }
+        }
 
-    private val requestPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()
-            ) { isGranted: Boolean ->
-                if (isGranted) {
-                    activity?.let {
-                        MaterialDialog.Builder(it)
-                            .setMessage(it.getString(R.string.importer_external_storage_permission_granted))
-                            .setCancelable(false)
-                            .setPositiveButton(it.getString(android.R.string.ok)) { dialogInterface, _ ->
-                                chooseFileFromLocal()
-                                dialogInterface.dismiss()
-                            }
-                            .build()
-                            .show()
-                    } ?: chooseFileFromLocal()
-                } else {
-                    activity?.let {
-                        MaterialDialog.Builder(it)
-                            .setMessage(it.getString(R.string.importer_external_storage_permission_denied))
-                            .setCancelable(false)
-                            .setPositiveButton(it.getString(android.R.string.ok)) { dialogInterface, _ ->
-                                dialogInterface.dismiss()
-                            }
-                            .build()
-                            .show()
-                    }
-                }
-            }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentDropboxchooserBinding.inflate(inflater, container, false)
         layout = binding.root
         return binding.root
@@ -87,9 +55,7 @@ class ImporterFragment : Fragment() {
     }
 
     private fun initViews() {
-        activity?.let {
-            progress = UiUtils.getProgressDialog(it, it.getString(R.string.loading_message))
-        }
+        progress = UiUtils.getProgressDialog(requireContext(), getString(R.string.loading_message))
         binding.btnChooser.setOnClickListener {
             chooseFile()
         }
@@ -100,55 +66,24 @@ class ImporterFragment : Fragment() {
     }
 
     private fun initObservers() {
-        viewModel.showResult.observe(viewLifecycleOwner, {
+        viewModel.showResult.observe(viewLifecycleOwner) {
             when (it) {
-                is Result.InProgress -> progress?.show()
+                is Result.InProgress -> progress.show()
                 is Result.Success -> {
-                    progress?.hide()
-                    Toast.makeText(activity, it.data, Toast.LENGTH_LONG).show()
+                    progress.hide()
+                    Toast.makeText(requireContext(), it.data, Toast.LENGTH_LONG).show()
                 }
+
                 is Result.Error -> {
-                    progress?.hide()
-                    Toast.makeText(activity, it.exception.message, Toast.LENGTH_LONG).show()
+                    progress.hide()
+                    Toast.makeText(requireContext(), it.exception.message, Toast.LENGTH_LONG).show()
                 }
             }
-        })
+        }
     }
 
     private fun chooseFile() {
-        val activity = activity ?: return
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            chooseFileFromLocal()
-        } else requestExternalStoragePermission()
-    }
-
-    private fun requestExternalStoragePermission() {
-        if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            activity?.let {
-                MaterialDialog.Builder(it)
-                    .setMessage(it.getString(R.string.importer_external_storage_access_required))
-                    .setCancelable(false)
-                    .setPositiveButton(it.getString(android.R.string.ok)) { dialogInterface, _ ->
-                        requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        dialogInterface.dismiss()
-                    }
-                    .build()
-                    .show()
-            } ?: requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        } else {
-            activity?.let {
-                MaterialDialog.Builder(it)
-                    .setMessage(it.getString(R.string.importer_external_storage_permission_not_available))
-                    .setCancelable(false)
-                    .setPositiveButton(it.getString(android.R.string.ok)) { dialogInterface, _ ->
-                        requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        dialogInterface.dismiss()
-                    }
-                    .build()
-                    .show()
-            } ?: requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        }
+        chooseFileFromLocal()
     }
 
     private fun chooseFileFromLocal() {
