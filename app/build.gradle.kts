91b62542c7a6d7cd4f9cb3e96e045b6f15f7d1af
Tony Yang <superzoro168@gmail.com>
2024-02-17 23:57:02 +0800

    migrate build to version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
     id("kotlin-parcelize")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -49,58 +49,40 @@ android {
 }
 
 dependencies {
-    val roomVersion = "2.5.0"
-    val constraintLayoutVersion = "2.0.4"
-    val recyclerviewVersion = "1.2.1"
-    val coroutinesCoreVersion = "1.3.9"
-    val coroutinesAndroidVersion = "1.3.9"
-    val materialVersion = "1.4.0"
-    val appcompatVersion = "1.3.0"
-    val junitVersion = "4.13.2"
-    val gsonVersion = "2.8.6"
-    val okhttpVersion = "4.9.1"
-    val ktxCoreVersion = "1.6.0"
-    val ktxFragmentVersion = "1.3.5"
-    val ktxViewModelVersion = "2.3.1"
-    val koinVersion = "2.2.2"
-    val viewpager2Version = "1.0.0"
-    val materialDialogVersion = "2.2.2"
-    val timberVersion = "5.0.1"
-
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesCoreVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesAndroidVersion")
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 
     // Android components
-    implementation("androidx.appcompat:appcompat:$appcompatVersion")
-    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
-    implementation("androidx.recyclerview:recyclerview:$recyclerviewVersion")
-    implementation("androidx.viewpager2:viewpager2:$viewpager2Version")
-    implementation("androidx.core:core-ktx:$ktxCoreVersion")
-    implementation("androidx.fragment:fragment-ktx:$ktxFragmentVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$ktxViewModelVersion")
-    implementation("com.google.android.material:material:$materialVersion")
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.viewpager2)
+    implementation(libs.core.ktx)
+    implementation(libs.fragment.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.material)
 
     // Room components
-    ksp("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-    androidTestImplementation("androidx.room:room-testing:$roomVersion")
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+    androidTestImplementation(libs.room.testing)
 
     // Testing
-    testImplementation("junit:junit:$junitVersion")
-    androidTestImplementation("junit:junit:$junitVersion")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junit.junit)
 
     // 3rd party libs
-    implementation("com.google.code.gson:gson:$gsonVersion")
-    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
-    implementation("dev.shreyaspatil.MaterialDialog:MaterialDialog:$materialDialogVersion")
+    implementation(libs.gson)
+    implementation(libs.okhttp)
+    implementation(libs.materialDialog)
 
     // koin
-    implementation("io.insert-koin:koin-core:$koinVersion")
-    implementation("io.insert-koin:koin-android:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-scope:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-viewmodel:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-fragment:$koinVersion")
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.scope)
+    implementation(libs.koin.androidx.viewmodel)
+    implementation(libs.koin.androidx.fragment)
 
-    implementation("com.jakewharton.timber:timber:$timberVersion")
+    implementation(libs.timber)
 }
